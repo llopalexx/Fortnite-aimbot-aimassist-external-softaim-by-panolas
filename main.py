168027c46edf823587b53eaa26a7242a545e26c2
Panolas <105129035+PanolasFN@users.noreply.github.com>
2022-12-20 21:18:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-https://fortnitesoftaim.com/ (anonymous and instant purchase, ALWAYS UPDATED)
+https://fortnitesoftaim.com/ (anonymous and instant purchase, ALWAYS UPDATED!)
 https://discord.gg/aimassist (discord to ask questions, purchase and hang out)
